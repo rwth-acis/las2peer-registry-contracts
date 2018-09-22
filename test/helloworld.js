ab651ecf674731f2170d6c080a705d64c7382cd8
Tom Janson <tom.janson@rwth-aachen.de>
2018-09-23 00:39:58 +0200

    nice, I figured it out

--- test/helloworld.js
+++ test/helloworld.js
@@ -1,7 +1,7 @@
 var HelloWorld = artifacts.require('HelloWorld')
 
 // random caveats:
-// - using chai expect(..).to.equal(..) only works, uh, sometimes
+// - using chai expect(..).to.equal(..) does not work for BigNumbers
 
 const v = 23 // some test value
 
@@ -9,17 +9,14 @@ const v = 23 // some test value
 contract('HelloWorld', accounts => {
     it('direct call of noop retuns tx which happens to use 21765 gas', () => {
         HelloWorld.deployed().then(i => i.noop(v)).then(r => assert.equal(r.receipt.gasUsed, 21765))
-        HelloWorld.deployed().then(i => i.noop(v)).then(r => expect(21765).to.equal(r.receipt.gasUsed))
     })
 
     it('noop function returns nothing', () => {
-        HelloWorld.deployed().then(i => i.noop.call(v)).then(r => assert.deepEqual(r, []))
-        HelloWorld.deployed().then(i => i.noop.call(v)).then(r => expect(r).to.deep.equal([]))
+        HelloWorld.deployed().then(i => i.noop.call(v)).then(expect([]).to.deep.equal)
     })
 
     it('executing pure function directly returns value', () => {
         HelloWorld.deployed().then(i => i.pureF(v)).then(r => assert.equal(r, v))
-        HelloWorld.deployed().then(i => i.pureF(v)).then(r => expect(23 == r).to.be.true)
     })
 
     it('calling pure function via call returns value', () => {
@@ -29,7 +26,6 @@ contract('HelloWorld', accounts => {
     it('setting and getting seems consistent', () => {
         HelloWorld.deployed().then(i => {
             i.setterF(v)
-            i.viewF(1 /* ignored anyway */).then(r => assert.equal(r, v))
             i.viewF(1 /* ignored anyway */).then(expect(v).to.equal)
         })
     })
