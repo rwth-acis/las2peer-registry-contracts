4c7aecc38d3efa1c2a2adbd8da491b3acee33d2e
Tom Janson <tom.janson@rwth-aachen.de>
2018-09-24 22:25:52 +0200

    BDD expect style tests

--- test/HelloWorldTest.js
+++ test/HelloWorldTest.js
@@ -3,7 +3,14 @@ const HelloWorldContract = artifacts.require('HelloWorld')
 // random caveats:
 // - using chai expect(..).to.equal(..) does not work for BigNumbers
 // - using chai expect(..).to.equal as partial also seems to sometimes fail mysteriously
+//
 // .. so let's just avoid using chai BDD style asserts for now
+//
+// TODO: possibly get around the BigNumber issue as described here:
+//       https://medium.com/@gus_tavo_guim/beautifying-your-smart-contract-tests-with-javascript-4d284efcb2e8
+//
+// TODO: possibly use chai-as-promised to simplify tests:
+//       https://github.com/domenic/chai-as-promised
 
 const v = 23 // some test value
 
@@ -15,23 +22,23 @@ contract('HelloWorld', accounts => {
     })
 
     it('direct call of noop retuns tx which happens to use 21765 gas', async () => {
-        assert.equal((await this.i.noop(v)).receipt.gasUsed, 21765)
+        expect((await this.i.noop(v)).receipt.gasUsed).to.equal(21765)
     })
 
     it('noop function returns nothing', async () => {
-        assert.deepEqual(await this.i.noop.call(v), [])
+        expect(await this.i.noop.call(v)).to.deep.equal([])
     })
 
     it('executing pure function directly returns value', async () => {
-        assert.equal(await this.i.pureF(v), v)
+        expect((await this.i.pureF(v)).toNumber()).to.equal(v)
     })
 
     it('calling pure function via call returns value', async () => {
-        assert.equal(await this.i.pureF.call(v), v)
+        expect((await this.i.pureF.call(v)).toNumber()).to.equal(v)
     })
 
     it('setting and getting seems consistent', async () => {
         await this.i.setterF(v)
-        expect(await this.i.viewF(1 /* dummy arg */), v)
+        expect((await this.i.viewF(1 /* dummy arg */)).toNumber()).to.equal(v)
     })
 })
