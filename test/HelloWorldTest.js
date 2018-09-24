cdec61407e7dacdf110d69d14f341f091544a284
Tom Janson <tom.janson@rwth-aachen.de>
2018-09-24 22:31:41 +0200

    should style test with custom chai
    rather than using the chai provided by truffle. OpenZeppelin does the
    same thing, e.g.,
    https://github.com/OpenZeppelin/openzeppelin-solidity/blob/4b21fcf5af6b411d6c39b0616e25d91c811d0768/test/token/ERC721/ERC721.test.js#L6-L8

--- test/HelloWorldTest.js
+++ test/HelloWorldTest.js
@@ -1,11 +1,8 @@
+require('chai')
+    .should()
+
 const HelloWorldContract = artifacts.require('HelloWorld')
 
-// random caveats:
-// - using chai expect(..).to.equal(..) does not work for BigNumbers
-// - using chai expect(..).to.equal as partial also seems to sometimes fail mysteriously
-//
-// .. so let's just avoid using chai BDD style asserts for now
-//
 // TODO: possibly get around the BigNumber issue as described here:
 //       https://medium.com/@gus_tavo_guim/beautifying-your-smart-contract-tests-with-javascript-4d284efcb2e8
 //
@@ -22,23 +19,23 @@ contract('HelloWorld', accounts => {
     })
 
     it('direct call of noop retuns tx which happens to use 21765 gas', async () => {
-        expect((await this.i.noop(v)).receipt.gasUsed).to.equal(21765)
+        (await this.i.noop(v)).receipt.gasUsed.should.equal(21765)
     })
 
     it('noop function returns nothing', async () => {
-        expect(await this.i.noop.call(v)).to.deep.equal([])
+        (await this.i.noop.call(v)).should.deep.equal([])
     })
 
     it('executing pure function directly returns value', async () => {
-        expect((await this.i.pureF(v)).toNumber()).to.equal(v)
+        (await this.i.pureF(v)).toNumber().should.equal(v)
     })
 
     it('calling pure function via call returns value', async () => {
-        expect((await this.i.pureF.call(v)).toNumber()).to.equal(v)
+        (await this.i.pureF.call(v)).toNumber().should.equal(v)
     })
 
     it('setting and getting seems consistent', async () => {
-        await this.i.setterF(v)
-        expect((await this.i.viewF(1 /* dummy arg */)).toNumber()).to.equal(v)
+        await this.i.setterF(v);
+        (await this.i.viewF(1 /* dummy arg */)).toNumber().should.equal(v)
     })
 })
