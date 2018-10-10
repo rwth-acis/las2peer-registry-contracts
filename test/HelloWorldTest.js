a95dfb98e98dac6dff9f64503dc96f8576240126
Tom Janson <tom.janson@rwth-aachen.de>
2018-10-10 16:36:20 +0200

    add tests for user registry

--- test/HelloWorldTest.js
+++ test/HelloWorldTest.js
@@ -6,29 +6,30 @@ const v = 23 // some test value
 
 // this contract function is provided by truffle, see their testing docs
 contract('HelloWorld', accounts => {
+    let helloWorld
+
     beforeEach(async () => {
-        let instance = await HelloWorldContract.deployed()
-        this.i = instance
+        helloWorld = await HelloWorldContract.deployed()
     })
 
     it('direct call of noop retuns tx which happens to use 21765 gas', async () => {
-        (this.i.noop(v)).should.eventually.nested.include({ 'receipt.gasUsed': 21765 })
+        (helloWorld.noop(v)).should.eventually.nested.include({ 'receipt.gasUsed': 21765 })
     })
 
     it('noop function returns nothing', async () => {
-        (this.i.noop.call(v)).should.eventually.deep.equal([])
+        (helloWorld.noop.call(v)).should.eventually.deep.equal([])
     })
 
     it('executing pure function directly returns value', async () => {
-        (this.i.pureF(v)).should.eventually.be.bignumber.equal(v)
+        (helloWorld.pureF(v)).should.eventually.be.bignumber.equal(v)
     })
 
     it('calling pure function via call returns value', async () => {
-        (this.i.pureF.call(v)).should.eventually.be.bignumber.equal(v)
+        (helloWorld.pureF.call(v)).should.eventually.be.bignumber.equal(v)
     })
 
     it('setting and getting seems consistent', async () => {
-        await this.i.setterF(v);
-        (this.i.viewF(1 /* dummy arg */)).should.eventually.be.bignumber.equal(v)
+        await helloWorld.setterF(v);
+        (helloWorld.viewF(1 /* dummy arg */)).should.eventually.be.bignumber.equal(v)
     })
 })
--- test/UserRegistryTest.js
+++ test/UserRegistryTest.js
@@ -1,4 +1,87 @@
-var UserRegistryContract = artifacts.require('HelloWorld')
+require('./support/setup.js')
+
+const UserRegistryContract = artifacts.require('UserRegistry')
+
+// NOTE: *returning* the promise assertions is important
+// at least I think that's what caused a bug that kept me busy for a while
+// it works sometimes, other times it does not (yeah, great fun ...)
+// see:
+//     "either return or notify(done) must be used with promise assertions"
+//     https://www.chaijs.com/plugins/chai-as-promised/
+
+const name = 'Alice'
+const agentId = 'an-agent-id'
+const text = 'lorem ipsum dolor sit amet'
 
 contract('UserRegistryContract', accounts => {
+    let userRegistry
+
+    beforeEach(async () => {
+        // using new to reset state, see
+        // https://github.com/trufflesuite/truffle/issues/727
+        // and in general
+        // https://stackoverflow.com/questions/29508444/why-should-mocha-test-cases-be-stateless
+        userRegistry = await UserRegistryContract.new()
+    })
+
+    it('empty (byte NULL) name is invalid', () =>
+        (userRegistry.nameIsValid('')).should.eventually.be.false
+    )
+
+    it('example name is valid', () =>
+        (userRegistry.nameIsValid(name)).should.eventually.be.true
+    )
+
+    it('example name is not taken', () =>
+        (userRegistry.nameIsTaken(name)).should.eventually.be.false
+    )
+
+    it('registration is possible, emits event, and makes name taken/unavailable', async () => {
+        let registrationResult = await userRegistry.register(name, agentId)
+
+        return Promise.all([
+            registrationResult.should.nested.include({
+                'logs[0].event': 'UserRegistered',
+                'logs[0].args.name': '0x416c696365000000000000000000000000000000000000000000000000000000' // web3.fromAscii(name, 64) // broken in web3 v0.2x.x
+            }),
+            (userRegistry.nameIsTaken(name)).should.eventually.be.true,
+            (userRegistry.nameIsAvailable(name)).should.eventually.be.false
+        ])
+    })
+
+    it('registration with duplicate name is not possible', async () => {
+        await userRegistry.register(name, agentId)
+        return (userRegistry.register(name, agentId)).should.be.rejected
+    })
+
+    it('setting a supplement and reading it from public var works', async () => {
+        await userRegistry.register(name, agentId)
+        await userRegistry.setSupplement(name, text)
+
+        let registryEntry = await userRegistry.users(name)
+        const supplementIndex = 3;
+        (web3.toAscii(registryEntry[supplementIndex])).should.equal(text)
+    })
+
+    it('username access restriction works (for setting supplement)', async () => {
+        await userRegistry.register(name, agentId, { from: accounts[1] })
+
+        // https://github.com/domenic/chai-as-promised#multiple-promise-assertions
+        return Promise.all([
+            (userRegistry.setSupplement(name, text, { from: accounts[0] })).should.be.rejected,
+            (userRegistry.setSupplement(name, text, { from: accounts[1] })).should.be.fulfilled
+        ])
+    })
+
+    it('name can be transferred', async () => {
+        await userRegistry.register(name, agentId, { from: accounts[0] })
+        let transferResult = await userRegistry.transfer(name, accounts[1])
+        return Promise.all([
+            (userRegistry.setSupplement(name, text, { from: accounts[1] })).should.be.fulfilled,
+            transferResult.should.nested.include({
+                'logs[0].event': 'UserTransferred',
+                'logs[0].args.name': '0x416c696365000000000000000000000000000000000000000000000000000000' // web3.fromAscii(name, 64) // broken
+            })
+        ])
+    })
 })
