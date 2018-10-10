70f8d520336d2bc14de471ac5d95b11ebcf99c6e
Tom Janson <tom.janson@rwth-aachen.de>
2018-10-10 16:37:25 +0200

    correct chai-as-promised tests
    also make them stateless

--- test/HelloWorldTest.js
+++ test/HelloWorldTest.js
@@ -9,27 +9,31 @@ contract('HelloWorld', accounts => {
     let helloWorld
 
     beforeEach(async () => {
-        helloWorld = await HelloWorldContract.deployed()
+        helloWorld = await HelloWorldContract.new()
     })
 
-    it('direct call of noop retuns tx which happens to use 21765 gas', async () => {
+    it('direct call of noop retuns tx which happens to use 21765 gas', () =>
         (helloWorld.noop(v)).should.eventually.nested.include({ 'receipt.gasUsed': 21765 })
-    })
+    )
 
-    it('noop function returns nothing', async () => {
-        (helloWorld.noop.call(v)).should.eventually.deep.equal([])
-    })
+    it('noop function returns nothing', () =>
+        (helloWorld.noop.call(v)).should.eventually.be.an('array').that.is.empty
+    )
 
-    it('executing pure function directly returns value', async () => {
-        (helloWorld.pureF(v)).should.eventually.be.bignumber.equal(v)
-    })
+    it('executing pure function directly returns value', () =>
+        (helloWorld.pureF(v)).should.eventually.bignumber.equal(v)
+    )
 
-    it('calling pure function via call returns value', async () => {
-        (helloWorld.pureF.call(v)).should.eventually.be.bignumber.equal(v)
-    })
+    it('calling pure function via call returns value', () =>
+        (helloWorld.pureF.call(v)).should.eventually.bignumber.equal(v)
+    )
 
     it('setting and getting seems consistent', async () => {
-        await helloWorld.setterF(v);
-        (helloWorld.viewF(1 /* dummy arg */)).should.eventually.be.bignumber.equal(v)
+        await helloWorld.setterF(v)
+        return (helloWorld.viewF(1 /* dummy arg */)).should.eventually.bignumber.equal(v)
     })
+
+    it('emitted event is visible in response log', () =>
+        (helloWorld.emitEvent(v)).should.eventually.nested.include({ 'logs[0].event': 'SimpleEvent' })
+    )
 })
