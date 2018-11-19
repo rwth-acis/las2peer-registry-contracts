dac597657d93e9f1868f13f03b36791531e25f95
Tom Janson <tom.janson@rwth-aachen.de>
2018-11-19 21:29:02 +0100

    add several network configs

--- truffle.js
+++ truffle.js
@@ -4,6 +4,24 @@ module.exports = {
             host: '127.0.0.1',
             port: 8545,
             network_id: '*'
+        },
+        cluster: {
+            host: '127.0.0.1',
+            port: 8545,
+            network_id: '456719',
+            gas: 5000000
+        },
+        gethdev: {
+            host: '127.0.0.1',
+            port: 8545,
+            network_id: '1337',
+            gas: 2000000
+        },
+        docker_boot: {
+            host: 'eth-bootstrap',
+            port: 8545,
+            network_id: '456719',
+            gas: 5000000
         }
     }
 }
