9babe46348f110b2c74f02281b56ff2c322d54ed
bengtson.lennart <lennart.bengtson@rwth-aachen.de>
2020-09-04 16:07:49 +0200

    Changed user identification from email to userId

--- contracts/ConsentRegistry.sol
+++ contracts/ConsentRegistry.sol
@@ -7,31 +7,31 @@ contract ConsentRegistry {
         // User user;
         address owner;
         uint256 timestamp;
-        bytes32 userEmail;
+        bytes32 userId;
         uint8[] consentLevels;
     }
 
-    // Mapping from user's email to user's consent
-    mapping(bytes32 => Consent) userMailToConsent;
+    // Mapping from user's ID to user's consent
+    mapping(bytes32 => Consent) userConsent;
 
-    function setConsent(bytes32 email, uint8[] memory consentLevels) public {
-        _createConsent(Consent(msg.sender, now, email, consentLevels));
+    function setConsent(bytes32 userId, uint8[] memory consentLevels) public {
+        _createConsent(Consent(msg.sender, now, userId, consentLevels));
     }
 
     function _createConsent(Consent memory consent) private {
-        userMailToConsent[consent.userEmail] = consent;
+        userConsent[consent.userId] = consent;
     }
 
-    function checkConsent(bytes32 email) public view returns(uint8[] memory) {
-        return userMailToConsent[email].consentLevels;
+    function checkConsent(bytes32 userId) public view returns(uint8[] memory) {
+        return userConsent[consent.userId].consentLevels;
     }
 
     // TODO Delegated function calls to enable setting the consent from a service without forcing the node operator to pay all fees.
 
     // ----------------- Testing functions -------------------
 
-    function getConsent(bytes32 email) public view returns(address, bytes32, uint8[] memory) {
-        Consent memory consent = userMailToConsent[email];
-        return (consent.owner, consent.userEmail, consent.consentLevels);
+    function getConsent(bytes32 userId) public view returns(address, bytes32, uint8[] memory) {
+        Consent memory consent = userConsent[userId];
+        return (consent.owner, consent.userId, consent.consentLevels);
     }
 }
