c1b4045b31ac1b6c41cae974fdf2240179fe00ba
bengtson.lennart <lennart.bengtson@rwth-aachen.de>
2020-09-07 19:02:52 +0200

    Delegated consent storage and only store consent if not already stored

--- contracts/ConsentRegistry.sol
+++ contracts/ConsentRegistry.sol
@@ -1,10 +1,9 @@
 pragma solidity ^0.5.0;
 
-// import {UserRegistry} from "./UserRegistry.sol";
+import { Delegation } from "./Delegation.sol";
 
 contract ConsentRegistry {
     struct Consent {
-        // User user;
         address owner;
         uint256 timestamp;
         bytes32 userId;
@@ -14,24 +13,44 @@ contract ConsentRegistry {
     // Mapping from user's ID to user's consent
     mapping(bytes32 => Consent) userConsent;
 
-    function setConsent(bytes32 userId, uint8[] memory consentLevels) public {
-        _createConsent(Consent(msg.sender, now, userId, consentLevels));
+    function hasStoredConsent(bytes32 userId) public view returns(bool){
+        Consent storage potentialConsent = userConsent[userId];
+        return potentialConsent.owner != address(0);
+    }
+
+    function storeConsent(bytes32 userId, uint8[] memory consentLevels) public {
+        if (!hasStoredConsent(userId)) {
+            _createConsent(Consent(msg.sender, now, userId, consentLevels));
+        }
     }
 
     function _createConsent(Consent memory consent) private {
         userConsent[consent.userId] = consent;
     }
 
-    function checkConsent(bytes32 userId) public view returns(uint8[] memory) {
+    function getUserConsentLevels(bytes32 userId) public view returns(uint8[] memory) {
         return userConsent[userId].consentLevels;
     }
 
-    // TODO Delegated function calls to enable setting the consent from a service without forcing the node operator to pay all fees.
-
-    // ----------------- Testing functions -------------------
-
-    function getConsent(bytes32 userId) public view returns(address, bytes32, uint8[] memory) {
+    function getConsent(bytes32 userId) public view returns(address, uint256, bytes32, uint8[] memory) {
         Consent memory consent = userConsent[userId];
-        return (consent.owner, consent.userId, consent.consentLevels);
+        return (consent.owner, consent.timestamp, consent.userId, consent.consentLevels);
+    }
+
+    function delegatedStoreConsent(
+        bytes32 userId,
+        uint8[] memory consentLevels,
+        address consentee,
+        bytes memory signature
+    )
+    public {
+        // first 8 chars of keccak("setConsent(bytes32,uint8[])")
+        bytes memory methodId = hex"c73c4234";
+        bytes memory args = abi.encode(userId, consentLevels);
+        Delegation.checkConsent(methodId, args, consentee, signature);
+
+        if (!hasStoredConsent(userId)) {
+            _createConsent(Consent(consentee, now, userId, consentLevels));
+        }
     }
 }
