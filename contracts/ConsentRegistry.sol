2c19d196718379827f2e6a49b3e14ec758345a59
bengtson.lennart <lennart.bengtson@rwth-aachen.de>
2020-09-07 19:34:14 +0200

    Add conditions

--- contracts/ConsentRegistry.sol
+++ contracts/ConsentRegistry.sol
@@ -13,30 +13,24 @@ contract ConsentRegistry {
     // Mapping from user's ID to user's consent
     mapping(bytes32 => Consent) userConsent;
 
+    // Checks for given userId (las2peer loginname) if a consent is stored already
     function hasStoredConsent(bytes32 userId) public view returns(bool){
         Consent storage potentialConsent = userConsent[userId];
         return potentialConsent.owner != address(0);
     }
 
+    // If no consent has been stored before, consent is stored for given user
     function storeConsent(bytes32 userId, uint8[] memory consentLevels) public {
-        if (!hasStoredConsent(userId)) {
-            _createConsent(Consent(msg.sender, now, userId, consentLevels));
-        }
+        if (hasStoredConsent(userId)) revert("Cannot overwrite stored consent.");
+        _createConsent(Consent(msg.sender, now, userId, consentLevels));
     }
 
+    // Stores consent Object in mapping
     function _createConsent(Consent memory consent) private {
         userConsent[consent.userId] = consent;
     }
 
-    function getUserConsentLevels(bytes32 userId) public view returns(uint8[] memory) {
-        return userConsent[userId].consentLevels;
-    }
-
-    function getConsent(bytes32 userId) public view returns(address, uint256, bytes32, uint8[] memory) {
-        Consent memory consent = userConsent[userId];
-        return (consent.owner, consent.timestamp, consent.userId, consent.consentLevels);
-    }
-
+    // Stores consent information on behalf of given user
     function delegatedStoreConsent(
         bytes32 userId,
         uint8[] memory consentLevels,
@@ -49,8 +43,21 @@ contract ConsentRegistry {
         bytes memory args = abi.encode(userId, consentLevels);
         Delegation.checkConsent(methodId, args, consentee, signature);
 
-        if (!hasStoredConsent(userId)) {
-            _createConsent(Consent(consentee, now, userId, consentLevels));
-        }
+        if (hasStoredConsent(userId)) revert("Cannot overwrite stored consent.");
+        _createConsent(Consent(consentee, now, userId, consentLevels));
     }
+
+    // Returns the consent levels stored for the given user
+    function getUserConsentLevels(bytes32 userId) public view returns(uint8[] memory) {
+        if (!hasStoredConsent(userId)) revert("No consent stored for this user.");
+        return userConsent[userId].consentLevels;
+    }
+
+    // Returns all consent information for the given user
+    function getConsent(bytes32 userId) public view returns(address, uint256, bytes32, uint8[] memory) {
+        if (!hasStoredConsent(userId)) revert("No consent stored for this user.");
+        Consent memory consent = userConsent[userId];
+        return (consent.owner, consent.timestamp, consent.userId, consent.consentLevels);
+    }
+
 }
