3157212b2eab3b1d6d05ad81f53b4ae8088e05e6
Rastoder Erdzan <rastoder.erdzan@rwth-aachen.de>
2021-05-18 14:39:30 +0200

    Adjust SC deployment

--- migrations/2_deploy_registry_contracts.js
+++ migrations/2_deploy_registry_contracts.js
@@ -1,5 +1,6 @@
 const Delegation = artifacts.require('Delegation')
 const UserRegistryContract = artifacts.require('UserRegistry')
+const GroupRegistryContract = artifacts.require('GroupRegistry')
 const ServiceRegistryContract = artifacts.require('ServiceRegistry')
 const ReputationRegistryContract = artifacts.require('ReputationRegistry')
 const CommunityTagIndexContract = artifacts.require('CommunityTagIndex')
@@ -12,15 +13,19 @@ module.exports = function (deployer) {
         deployer.link(Delegation, UserRegistryContract)
         deployer.link(Delegation, ServiceRegistryContract)
         deployer.link(Delegation, ReputationRegistryContract)
+        deployer.link(Delegation, GroupRegistryContract)
     }).then(function() {
         // dependent deploy exactly like the example here:
         // https://truffleframework.com/docs/truffle/getting-started/running-migrations#deployer
-        return deployer.deploy(UserRegistryContract).then( function () { 
+        return deployer.deploy(UserRegistryContract).then( function () {
             return deployer.deploy(ReputationRegistryContract, UserRegistryContract.address)
         });
     }).then(function() {
-        return deployer.deploy(UserRegistryContract, {overwrite: false}).then( function () { 
+        return deployer.deploy(UserRegistryContract, {overwrite: false}).then( function () {
             return deployer.deploy(ServiceRegistryContract, UserRegistryContract.address)
-        });
+        }).then(function() {
+            return deployer.deploy(GroupRegistryContract, {overwrite: false}).then( function () {
+                return deployer.deploy(ServiceRegistryContract, GroupRegistryContract.address)
+            });;
     });
 }
